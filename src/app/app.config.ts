2387a45dabd08aaa3af042927edc5a4811e04c2d
Mary Imevbore <maryimevbore@Marys-MacBook-Pro-2.local>
2025-12-03 20:56:40 -0500

    to fix build error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-54c44","appId":"1:110183515658:web:8dadf4084d44cfe64d81f3","storageBucket":"friendlychat-54c44.firebasestorage.app","apiKey":"AIzaSyAlczTBE8O8SoWqh1MTiogy6K4or7XrzMI","authDomain":"friendlychat-54c44.firebaseapp.com","messagingSenderId":"110183515658","projectNumber":"110183515658","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
+    provideRouter(routes)
   ],
 };
