e135de32ad4b1c66911008c73dabb2049c34e1b7
Mary Imevbore <maryimevbore@Marys-MacBook-Pro-2.local>
2025-12-04 20:01:45 -0500

    firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "API_KEY",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.firebasestorage.app",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+  },
+};
