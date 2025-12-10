1a3c001dd8b9d8613335268a53737ef5d042bed6
Mary Imevbore <maryimevbore@Marys-MacBook-Pro-2.local>
2025-12-10 14:20:48 -0500

    config fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyAlczTBE8O8SoWqh1MTiogy6K4or7XrzMI",
+    authDomain: "friendlychat-54c44.firebaseapp.com",
+    projectId: "friendlychat-54c44",
+    storageBucket: "friendlychat-54c44.firebasestorage.app",
+    messagingSenderId: "110183515658",
+    appId: "1:110183515658:web:8dadf4084d44cfe64d81f3"
   },
 };
