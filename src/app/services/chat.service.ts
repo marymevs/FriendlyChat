56fdac284bb99638d67093d3ef7a5e0f8a2297dc
Mary Imevbore <maryimevbore@Marys-MacBook-Pro-2.local>
2025-12-10 19:06:22 -0500

    reversed order

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -146,7 +146,7 @@ addMessage = async (
   // Loads chat messages history and listens for upcoming ones.
 loadMessages = () => {
   // Create the query to load the last 12 messages and listen for new ones.
-  const recentMessagesQuery = query(collection(this.firestore, 'messages'), orderBy('timestamp', 'asc'), limit(12));
+  const recentMessagesQuery = query(collection(this.firestore, 'messages'), orderBy('timestamp', 'desc'), limit(12));
   // Start listening to the query.
   return collectionData(recentMessagesQuery);
 }
